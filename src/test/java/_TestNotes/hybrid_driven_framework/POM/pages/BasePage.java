f5a5dc250f575c13ee5c7bc2bc639c76039b003a
Jim Moua <54656947+jimmoua1@users.noreply.github.com>
2022-01-14 07:20:08 -0800

    Delete src/test/java/_TestNotes directory

--- src/test/java/_TestNotes/hybrid_driven_framework/POM/pages/BasePage.java
+++
@@ -1,9 +0,0 @@
-package _TestNotes.hybrid_driven_framework.POM.pages;
-
-public class BasePage {
-
-
-    // Like BaseTest can make BasePage here for repetitive code
-    // for POM(Page Object Model)
-
-}
